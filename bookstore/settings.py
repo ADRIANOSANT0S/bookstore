57e9c83b03c4bc7205ca909e9e84304233874930
portfolio <adrianoopereiira@hotmail.com>
2025-02-10 07:36:05 -0300

    feat: finish settings for project routes and add viewsets

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'order',
-    'product'
+    "django_extensions",
+    "order",
+    "product",
 ]
 
 MIDDLEWARE = [
--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -4,7 +4,7 @@ from product.models import Product
 from product.serializers import ProductSerializer
 
 
-class ProductViewset(ModelViewSet):
+class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
